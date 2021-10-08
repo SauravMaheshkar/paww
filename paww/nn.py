9143dd2562f5e03b0b4c97a46e69d20a4a381117
SauravMaheshkar <sauravvmaheshkar@gmail.com>
2021-10-09 05:04:57 +0530

    Hopefully add ResNet support

--- paww/nn.py
+++ paww/nn.py
@@ -8,9 +8,17 @@ class Model(nn.Module):
     def __init__(self, cfg: dict, pretrained: bool = False):
         super(Model, self).__init__()
         self.cfg = cfg
-        self.model = timm.create_model(self.cfg["model_name"], pretrained=pretrained)
-        self.n_features = self.model.classifier.in_features
-        self.model.classifier = nn.Identity()
+        if cfg["model_type"] == "EfficientNet":
+            self.model = timm.create_model(
+                self.cfg["model_name"], pretrained=pretrained
+            )
+            self.n_features = self.model.classifier.in_features
+            self.model.classifier = nn.Identity()
+        elif cfg["model_type"] == "ResNet":
+            self.model = timm.create_model(
+                self.cfg["model_name"], pretrained=pretrained
+            )
+            self.n_features = self.model.fc.in_features
         self.fc = nn.Linear(self.n_features, self.cfg["target_size"])
 
     def feature(self, image):
--- tests/test_model.py
+++ tests/test_model.py
@@ -3,9 +3,20 @@ import torch.nn as nn
 from paww.nn import Model
 
 
-def test_model():
+def test_efficient():
 
-    cfg = dict(model_name="tf_efficientnet_b0_ns", target_size=1)
+    cfg = dict(
+        model_name="tf_efficientnet_b0_ns", model_type="EfficientNet", target_size=1
+    )
+
+    net = Model(cfg, pretrained=False)
+
+    assert isinstance(net, nn.Module)
+
+
+def test_resnet():
+
+    cfg = dict(model_name="resnet18", model_type="ResNet", target_size=1)
 
     net = Model(cfg, pretrained=False)
 
